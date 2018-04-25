ee411bd6510b075164792b44e8866ef8b469708f
Valentin Laurin <me@vlaurin.eu>
2018-04-25 12:05:59 +0100

    Try to destroy old DB, test lock

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -12,24 +12,24 @@ module "user-profile-api" {
   is_frontend = false
 
   app_settings = {
-    USER_PROFILE_DB_HOST        = "${module.postgres-user-profile.host_name}"
-    USER_PROFILE_DB_PORT        = "${module.postgres-user-profile.postgresql_listen_port}"
-    USER_PROFILE_DB_NAME        = "${module.postgres-user-profile.postgresql_database}"
-    USER_PROFILE_DB_USERNAME    = "${module.postgres-user-profile.user_name}"
-    USER_PROFILE_DB_PASSWORD    = "${module.postgres-user-profile.postgresql_password}"
+    USER_PROFILE_DB_HOST        = "${module.db-user-profile.host_name}"
+    USER_PROFILE_DB_PORT        = "${module.db-user-profile.postgresql_listen_port}"
+    USER_PROFILE_DB_NAME        = "${module.db-user-profile.postgresql_database}"
+    USER_PROFILE_DB_USERNAME    = "${module.db-user-profile.user_name}"
+    USER_PROFILE_DB_PASSWORD    = "${module.db-user-profile.postgresql_password}"
     IDAM_S2S_URL = "${var.s2s_url}"
     USER_PROFILE_S2S_AUTHORISED_SERVICES = "${var.authorised-services}"
   }
 
 }
 
-module "postgres-user-profile" {
-  source              = "git@github.com:contino/moj-module-postgres?ref=master"
-  product             = "${var.product}-user-profile"
-  location            = "West Europe"
-  env                 = "${var.env}"
-  postgresql_user     = "ccd"
-}
+//module "postgres-user-profile" {
+//  source              = "git@github.com:contino/moj-module-postgres?ref=master"
+//  product             = "${var.product}-user-profile"
+//  location            = "West Europe"
+//  env                 = "${var.env}"
+//  postgresql_user     = "ccd"
+//}
 
 module "db-user-profile" {
   source = "git@github.com:hmcts/moj-module-postgres?ref=cnp-449-tactical"
