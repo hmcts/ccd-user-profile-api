79499b86502f279791d04b7c689bbcfc74b88dea
Valentin Laurin <me@vlaurin.eu>
2018-04-25 12:56:27 +0100

    Both DB with UK switch off

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -12,26 +12,26 @@ module "user-profile-api" {
   is_frontend = false
 
   app_settings = {
-    USER_PROFILE_DB_HOST        = "${module.db-user-profile.host_name}"
-    USER_PROFILE_DB_PORT        = "${module.db-user-profile.postgresql_listen_port}"
-    USER_PROFILE_DB_NAME        = "${module.db-user-profile.postgresql_database}"
-    USER_PROFILE_DB_USERNAME    = "${module.db-user-profile.user_name}"
-    USER_PROFILE_DB_PASSWORD    = "${module.db-user-profile.postgresql_password}"
+    USER_PROFILE_DB_HOST        = "${var.use_uk_db != "true" ? module.postgres-user-profile.host_name : module.user-profile-db.host_name}"
+    USER_PROFILE_DB_PORT        = "${var.use_uk_db != "true" ? module.postgres-user-profile.postgresql_listen_port : module.user-profile-db.postgresql_listen_port}"
+    USER_PROFILE_DB_NAME        = "${var.use_uk_db != "true" ? module.postgres-user-profile.postgresql_database : module.user-profile-db.postgresql_database}"
+    USER_PROFILE_DB_USERNAME    = "${var.use_uk_db != "true" ? module.postgres-user-profile.user_name : module.user-profile-db.user_name}"
+    USER_PROFILE_DB_PASSWORD    = "${var.use_uk_db != "true" ? module.postgres-user-profile.postgresql_password : module.user-profile-db.postgresql_password}"
     IDAM_S2S_URL = "${var.s2s_url}"
     USER_PROFILE_S2S_AUTHORISED_SERVICES = "${var.authorised-services}"
   }
 
 }
 
-//module "postgres-user-profile" {
-//  source              = "git@github.com:contino/moj-module-postgres?ref=master"
-//  product             = "${var.product}-user-profile"
-//  location            = "West Europe"
-//  env                 = "${var.env}"
-//  postgresql_user     = "ccd"
-//}
+module "postgres-user-profile" {
+  source              = "git@github.com:contino/moj-module-postgres?ref=master"
+  product             = "${var.product}-user-profile"
+  location            = "West Europe"
+  env                 = "${var.env}"
+  postgresql_user     = "ccd"
+}
 
-module "db-user-profile" {
+module "user-profile-db" {
   source = "git@github.com:hmcts/moj-module-postgres?ref=cnp-449-tactical"
   product = "${local.app_full_name}-postgres-db"
   location = "${var.location}"
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -33,6 +33,11 @@ variable "database_name" {
   default = "ccd_user_profile"
 }
 
+variable "use_uk_db" {
+  type = "string"
+  default = "false"
+}
+
 ////////////////////////////////
 // S2S
 ////////////////////////////////
