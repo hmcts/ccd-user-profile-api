f5ea359348f1100093b53489f1c3e421a66ff6a3
Valentin Laurin <me@vlaurin.eu>
2018-04-26 18:51:51 +0100

    Revert test failure

--- src/aat/java/uk/gov/hmcts/ccd/userprofile/tests/functional/UserProfileTest.java
+++ src/aat/java/uk/gov/hmcts/ccd/userprofile/tests/functional/UserProfileTest.java
@@ -27,7 +27,7 @@ class UserProfileTest extends BaseTest {
             .when()
             .post("/user-profile/users")
             .then()
-            .statusCode(500);
+            .statusCode(400);
     }
 
 }
