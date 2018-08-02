c8ef8a7f8aad3d97a0fac6bc77bdc409f4dd8141
Lam <daniel.2.lam@atos.net>
2018-08-02 12:30:03 +0100

    RDM-2418: Map existing "update" operation to new endpoint
    Clone REST endpoint for `PUT` operation and map to the `/users` path.

--- src/main/java/uk/gov/hmcts/ccd/endpoint/userprofile/UserProfileController.java
+++ src/main/java/uk/gov/hmcts/ccd/endpoint/userprofile/UserProfileController.java
@@ -6,6 +6,7 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,7 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 import uk.gov.hmcts.ccd.AppInsights;
 import uk.gov.hmcts.ccd.domain.model.UserProfile;
 import uk.gov.hmcts.ccd.domain.service.FindAllUserProfilesOperation;
+import uk.gov.hmcts.ccd.domain.service.UserProfileOperation;
 
+import javax.transaction.Transactional;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.List;
@@ -23,12 +26,15 @@ import java.util.Optional;
 @RestController
 class UserProfileController {
     private final FindAllUserProfilesOperation findAllUserProfilesOperation;
+    private final UserProfileOperation userProfileOperation;
     private final AppInsights appInsights;
 
     @Autowired
     public UserProfileController(FindAllUserProfilesOperation findAllUserProfilesOperation,
+                                 UserProfileOperation userProfileOperation,
                                  AppInsights appInsights) {
         this.findAllUserProfilesOperation = findAllUserProfilesOperation;
+        this.userProfileOperation = userProfileOperation;
         this.appInsights = appInsights;
     }
 
@@ -51,4 +57,15 @@ class UserProfileController {
         return userProfiles;
     }
 
+    @Transactional
+    @RequestMapping(value = "/users", method = RequestMethod.PUT)
+    @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation(value = "Update a new User Profile",
+        notes = "a user profile or jurisdiction is created if it does not exist")
+    @ApiResponses(value = {
+        @ApiResponse(code = 201, message = "Updated User Profile defaults")
+    })
+    public void populateUserProfiles(@RequestBody final List<UserProfile> userProfiles) {
+        userProfileOperation.execute(userProfiles);
+    }
 }
