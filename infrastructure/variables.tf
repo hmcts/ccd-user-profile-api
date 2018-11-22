bc05eacf3e18b0b6fb28d3df407f8678a2456409
Adrian Cho <waihung.cho@hmcts.net>
2018-11-22 10:07:38 +0000

    Rebase

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -2,6 +2,10 @@ variable "product" {
   type    = "string"
 }
 
+variable "raw_product" {
+  default = "ccd" // jenkins-library overrides product for PRs and adds e.g. pr-118-ccd
+}
+
 variable "component" {
   type    = "string"
 }
@@ -19,7 +23,23 @@ variable "subscription" {
   type = "string"
 }
 
+variable "capacity" {
+  default = "1"
+}
+
 variable "ilbIp"{}
+
+variable "asp_name" {
+  type = "string"
+  description = "App Service Plan (ASP) to use for the webapp, 'use_shared' to make use of the shared ASP"
+  default = "use_shared"
+}
+
+variable "asp_rg" {
+  type = "string"
+  description = "App Service Plan (ASP) resource group for 'asp_name', 'use_shared' to make use of the shared resource group"
+  default = "use_shared"
+}
 
 variable "tenant_id" {
   description = "(Required) The Azure Active Directory tenant ID that should be used for authenticating requests to the key vault. This is usually sourced from environemnt variables and not normally required to be specified."
@@ -30,11 +50,27 @@ variable "jenkins_AAD_objectId" {
   description                 = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
 
-variable "s2s_url" {
-  default = "http://betaDevBccidamS2SLB.reform.hmcts.net"
+////////////////////////////////
+// Database
+////////////////////////////////
+
+variable "postgresql_user" {
+  default = "ccd"
 }
+
+variable "database_name" {
+  default = "ccd_user_profile"
+}
+
+////////////////////////////////
+// S2S
+////////////////////////////////
 
 variable "authorised-services" {
   type    = "string"
-  default = "ccd_data,ccd_definition"
+  default = "ccd_data,ccd_definition,ccd_admin"
+}
+
+variable "common_tags" {
+  type = "map"
 }
