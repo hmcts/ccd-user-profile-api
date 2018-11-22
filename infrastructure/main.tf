92c7113ad4db1bb79864438ad13fe0f6856a89a6
Adrian Cho <waihung.cho@hmcts.net>
2018-11-22 10:11:16 +0000

    Rebase

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -44,6 +44,7 @@ module "user-profile-api" {
 
   app_settings = {
 <<<<<<< HEAD
+<<<<<<< HEAD
     USER_PROFILE_DB_HOST        = "${module.user-profile-db.host_name}"
     USER_PROFILE_DB_PORT        = "${module.user-profile-db.postgresql_listen_port}"
     USER_PROFILE_DB_NAME        = "${module.user-profile-db.postgresql_database}"
@@ -59,6 +60,13 @@ module "user-profile-api" {
     USER_PROFILE_DB_NAME        = "${module.db-user-profile.postgresql_database}"
     USER_PROFILE_DB_USERNAME    = "${module.db-user-profile.user_name}"
     USER_PROFILE_DB_PASSWORD    = "${module.db-user-profile.postgresql_password}"
+=======
+    USER_PROFILE_DB_HOST        = "${var.use_uk_db != "true" ? module.postgres-user-profile.host_name : module.user-profile-db.host_name}"
+    USER_PROFILE_DB_PORT        = "${var.use_uk_db != "true" ? module.postgres-user-profile.postgresql_listen_port : module.user-profile-db.postgresql_listen_port}"
+    USER_PROFILE_DB_NAME        = "${var.use_uk_db != "true" ? module.postgres-user-profile.postgresql_database : module.user-profile-db.postgresql_database}"
+    USER_PROFILE_DB_USERNAME    = "${var.use_uk_db != "true" ? module.postgres-user-profile.user_name : module.user-profile-db.user_name}"
+    USER_PROFILE_DB_PASSWORD    = "${var.use_uk_db != "true" ? module.postgres-user-profile.postgresql_password : module.user-profile-db.postgresql_password}"
+>>>>>>> 79499b8... Both DB with UK switch off
     IDAM_S2S_URL = "${var.s2s_url}"
 >>>>>>> ee411bd... Try to destroy old DB, test lock
     USER_PROFILE_S2S_AUTHORISED_SERVICES = "${var.authorised-services}"
@@ -66,6 +74,7 @@ module "user-profile-api" {
 
 }
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 module "user-profile-db" {
   source = "git@github.com:hmcts/cnp-module-postgres?ref=master"
@@ -77,8 +86,17 @@ module "user-profile-db" {
 //  env                 = "${var.env}"
 //  postgresql_user     = "ccd"
 //}
+=======
+module "postgres-user-profile" {
+  source              = "git@github.com:contino/moj-module-postgres?ref=master"
+  product             = "${var.product}-user-profile"
+  location            = "West Europe"
+  env                 = "${var.env}"
+  postgresql_user     = "ccd"
+}
+>>>>>>> 79499b8... Both DB with UK switch off
 
-module "db-user-profile" {
+module "user-profile-db" {
   source = "git@github.com:hmcts/moj-module-postgres?ref=cnp-449-tactical"
 >>>>>>> ee411bd... Try to destroy old DB, test lock
   product = "${local.app_full_name}-postgres-db"
