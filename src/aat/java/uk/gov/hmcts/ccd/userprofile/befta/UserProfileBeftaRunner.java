cc61b37bccf3534b408d338aaaef3df3000c4508
RBaker <rebecca.baker@hmcts.net>
2020-02-12 15:49:44 +0000

    RDM-6492 WIP updated to add the runner class

---
+++ src/aat/java/uk/gov/hmcts/ccd/userprofile/befta/UserProfileBeftaRunner.java
@@ -0,0 +1,21 @@
+package uk.gov.hmcts.ccd.userprofile.befta;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import uk.gov.hmcts.befta.BeftaMain;
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = "json:target/cucumber.json",
+    glue = "uk.gov.hmcts.befta.player",
+    features = {"classpath:features"})
+public class UserProfileBeftaRunner {
+    public static void setUp() {
+        BeftaMain.setUp(new UserProfileTestAutomationAdapter());
+    }
+    @AfterClass
+    public static void tearDown() {
+        BeftaMain.tearDown();
+    }
+}
