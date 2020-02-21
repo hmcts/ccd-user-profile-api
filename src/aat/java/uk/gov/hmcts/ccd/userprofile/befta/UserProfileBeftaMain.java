1ef8d395e067ed285f332659603715298e8f7e79
andrewf <andrzej.folga@hmcts.net>
2020-02-21 13:28:19 +0000

    RDM-6496 Remove UserProfileTestAutomationAdapter and use the default one from fw.

--- src/aat/java/uk/gov/hmcts/ccd/userprofile/befta/UserProfileBeftaMain.java
+++ src/aat/java/uk/gov/hmcts/ccd/userprofile/befta/UserProfileBeftaMain.java
@@ -1,11 +1,12 @@
 package uk.gov.hmcts.ccd.userprofile.befta;
 
 import uk.gov.hmcts.befta.BeftaMain;
+import uk.gov.hmcts.befta.DefaultTestAutomationAdapter;
 
 public class UserProfileBeftaMain extends BeftaMain {
 
     public static void main(String[] args) {
-        setTaAdapter(new UserProfileTestAutomationAdapter());
+        setTaAdapter(new DefaultTestAutomationAdapter());
         BeftaMain.main(args);
     }
 
