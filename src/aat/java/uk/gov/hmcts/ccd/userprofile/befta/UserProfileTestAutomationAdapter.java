00df902d41cd003f672bed7e2ab7efea1bbe2124
andrewf <andrzej.folga@hmcts.net>
2020-02-21 11:13:59 +0000

    RDM-6496 No need for relaxed https validation here.

--- src/aat/java/uk/gov/hmcts/ccd/userprofile/befta/UserProfileTestAutomationAdapter.java
+++ src/aat/java/uk/gov/hmcts/ccd/userprofile/befta/UserProfileTestAutomationAdapter.java
@@ -1,13 +1,10 @@
 package uk.gov.hmcts.ccd.userprofile.befta;
 
-import io.restassured.RestAssured;
 import uk.gov.hmcts.befta.DefaultTestAutomationAdapter;
 
 public class UserProfileTestAutomationAdapter extends DefaultTestAutomationAdapter {
 
     @Override
-    public void doLoadTestData() {
-        RestAssured.useRelaxedHTTPSValidation();
-    }
+    public void doLoadTestData() {}
 
 }
