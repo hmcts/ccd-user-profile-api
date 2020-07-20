4105ed8fdac08aa927d0f82251b56f183a0bf924
mutlu <mutlu.sancaktutar@hmcts.net>
2020-07-20 14:39:19 +0100

    cleanup legacy tests

---
+++ src/aat/java/uk/gov/hmcts/ccd/userprofile/DummyTest.java
@@ -0,0 +1,16 @@
+package uk.gov.hmcts.ccd.userprofile;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+public class DummyTest {
+
+    @Test
+    @DisplayName("Dummy Test To Satisfy JUnit Task With Non-Empty Results")
+    @Tag("smoke")
+    void dummyTestToSatisfyJUnitTaskWithNonEmptyResults() {
+        System.out.println("Dummy Test To Satisfy JUnit Task With Non-Empty Results");
+    }
+
+}
