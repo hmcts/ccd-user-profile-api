794fe56019dd308eae6f32770c9459598bd2524e
kiran.yenigala@hmcts.net <Yrlks1091327~>
2020-08-18 10:31:27 +0100

    RDM-9250: Removed un-used LOG

--- src/main/java/uk/gov/hmcts/ccd/domain/service/FindUserProfileOperation.java
+++ src/main/java/uk/gov/hmcts/ccd/domain/service/FindUserProfileOperation.java
@@ -1,20 +1,16 @@
 package uk.gov.hmcts.ccd.domain.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.ccd.data.userprofile.UserProfileRepository;
 import uk.gov.hmcts.ccd.domain.model.UserProfile;
 import uk.gov.hmcts.ccd.endpoint.exception.NotFoundException;
 
-import java.util.Optional;
-
 @Service
 public class FindUserProfileOperation {
 
     private final UserProfileRepository userProfileRepository;
-    private static final Logger LOG = LoggerFactory.getLogger(FindUserProfileOperation.class);
 
     @Autowired
     public FindUserProfileOperation(UserProfileRepository userProfileRepository) {
