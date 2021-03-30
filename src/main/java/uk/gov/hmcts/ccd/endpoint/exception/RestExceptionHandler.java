330732545bb2bacc2e12911b426c1abae7f2a3db
Tom Elliott <tomxelliott@hotmail.co.uk>
2021-03-30 16:47:21 +0100

    RDM-9182: Add exception handler for SQL errors that do not expose SQL grammar messages

--- src/main/java/uk/gov/hmcts/ccd/endpoint/exception/RestExceptionHandler.java
+++ src/main/java/uk/gov/hmcts/ccd/endpoint/exception/RestExceptionHandler.java
@@ -12,6 +12,8 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import uk.gov.hmcts.ccd.AppInsights;
 
+import java.sql.SQLException;
+
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     private static final Logger LOG = LoggerFactory.getLogger(RestExceptionHandler.class);
@@ -38,6 +40,15 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
             return handleExceptionInternal(e, errorMsg, headers, HttpStatus.NOT_FOUND, request);
         }
         LOG.warn("exception with message {}, {}, {}", errorMsg, HttpStatus.INTERNAL_SERVER_ERROR, request, e);
+        return handleExceptionInternal(e, errorMsg, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+    }
+
+    @ExceptionHandler(SQLException.class)
+    protected ResponseEntity<Object> handleSQLException(final SQLException e, final WebRequest request) {
+        final String errorMsg = "SQL Exception thrown during API operation, " + HttpStatus.INTERNAL_SERVER_ERROR;
+        appInsights.trackException(e);
+
+        LOG.warn(errorMsg);
         return handleExceptionInternal(e, errorMsg, new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
     }
 }
