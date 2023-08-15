d3491d6646ad0cb9c80eff1a435c5c599e61a0d5
RebeccaBaker <38425793+RebeccaBaker@users.noreply.github.com>
2023-08-15 17:13:29 +0100

    Update app-insights.tf

--- infrastructure/app-insights.tf
+++ infrastructure/app-insights.tf
@@ -1,3 +1,13 @@
+data "azurerm_application_insights" "ai" {
+  name                = "${var.product}-${var.env}"
+  resource_group_name = "${var.product}-shared-${var.env}"
+}
+
+data "azurerm_key_vault" "key_vault" {
+  name                = "${var.product}-${var.env}"
+  resource_group_name = "${var.product}-shared-${var.env}"
+}
+
 resource "azurerm_key_vault_secret" "app_insights_connection_string" {
   name         = "app-insights-connection-string"
   value        = azurerm_application_insights.appinsights.connection_string
