077b33bd052c760d1a02d9652d7e140d401236c9
RebeccaBaker <38425793+RebeccaBaker@users.noreply.github.com>
2023-08-15 17:37:53 +0100

    Update app-insights.tf

--- infrastructure/app-insights.tf
+++ infrastructure/app-insights.tf
@@ -10,6 +10,6 @@ data "azurerm_key_vault" "key_vault" {
 
 resource "azurerm_key_vault_secret" "app_insights_connection_string" {
   name         = "app-insights-connection-string"
-  value        = azurerm_application_insights.appinsights.connection_string
-  key_vault_id = module.key-vault.key_vault_id
+  value        = data.azurerm_application_insights.ai.connection_string
+  key_vault_id = data.azurerm_key_vault.key_vault.id
 }
