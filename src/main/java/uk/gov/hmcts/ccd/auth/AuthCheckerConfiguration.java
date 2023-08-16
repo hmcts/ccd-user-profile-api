674e09d01f19bae22b33c2c2731afed87a261100
Ben Lang <132359359+lang-ben@users.noreply.github.com>
2023-08-16 13:17:59 +0100

    Bump java-logging version

--- src/main/java/uk/gov/hmcts/ccd/auth/AuthCheckerConfiguration.java
+++ src/main/java/uk/gov/hmcts/ccd/auth/AuthCheckerConfiguration.java
@@ -1,9 +1,9 @@
 package uk.gov.hmcts.ccd.auth;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.Collection;
 import java.util.function.Function;
 
