f52beb19f3a44a51c22a154ab7e67cd51ba6ad4b
Muhammad <muhammad.umerji@hmcts.net>
2023-10-18 13:45:39 +0100

    Updated variables.tf, main.tf, and state.tf for PostgreSQL v15 implementation

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -1,17 +1,10 @@
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+  alias                      = "postgres_network"
+  subscription_id            = var.aks_subscription_id
+}
+
 terraform {
   backend "azurerm" {}
 
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-    random = {
-      source = "hashicorp/random"
-    }
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "1.6.0"
-    }
-  }
-}
