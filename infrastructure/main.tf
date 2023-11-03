6fa948b676693de49692e22670008b8b3c80160c
Muhammad <muhammad.umerji@hmcts.net>
2023-11-03 16:09:36 +0000

    Add subnet_suffix for expanded subnet in Terraform config

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -79,6 +79,8 @@ module "postgresql_v15" {
     azurerm.postgres_network = azurerm.postgres_network
   }
 
+  subnet_suffix = "expanded"
+  
   admin_user_object_id = var.jenkins_AAD_objectId
   business_area        = "cft"
   common_tags          = var.common_tags
