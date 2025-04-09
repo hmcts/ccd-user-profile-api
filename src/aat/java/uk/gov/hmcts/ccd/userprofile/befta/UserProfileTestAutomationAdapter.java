68a4843c2fc3d10d6df09ce274d29a427217e9f2
Dinesh Patel <dinesh1.patel@btinternet.com>
2025-04-09 11:22:36 +0100

    Update UserProfileTestAutomationAdapter.java
    Modified to calculate scenarioContext

--- src/aat/java/uk/gov/hmcts/ccd/userprofile/befta/UserProfileTestAutomationAdapter.java
+++ src/aat/java/uk/gov/hmcts/ccd/userprofile/befta/UserProfileTestAutomationAdapter.java
@@ -1,7 +1,15 @@
 package uk.gov.hmcts.ccd.userprofile.befta;
 
 import uk.gov.hmcts.befta.DefaultTestAutomationAdapter;
+import uk.gov.hmcts.befta.player.BackEndFunctionalTestScenarioContext;
 
 public class UserProfileTestAutomationAdapter extends DefaultTestAutomationAdapter {
 
+    @Override
+    public synchronized Object calculateCustomValue(BackEndFunctionalTestScenarioContext scenarioContext, Object key) {
+        if (key.toString().startsWith("no_dynamic_injection_")) {
+            return key.toString().replace("no_dynamic_injection_","");
+        }
+        return super.calculateCustomValue(scenarioContext, key);
+    }
 }
