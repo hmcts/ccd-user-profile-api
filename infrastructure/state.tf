9f6b5ff61778f6c65b04004c27955e19224532df
James Collett <89016121+JamesCollettCGI@users.noreply.github.com>
2026-08-25 16:20:09 +0100

    Update state.tf to azurerm 5.0 compatibility

--- infrastructure/state.tf
+++ infrastructure/state.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
   features {}
-  skip_provider_registration = true
-  alias                      = "postgres_network"
-  subscription_id            = var.aks_subscription_id
+  resource_provider_registrations = "none"
+  alias                           = "postgres_network"
+  subscription_id                 = var.aks_subscription_id
 }
 
 terraform {
